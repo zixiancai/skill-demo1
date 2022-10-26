d927615fc526d1d31e240143e696e7b7b91bea1c
Joe Politz <joe@cs.brown.edu>
2022-10-25 21:49:41 -0700

    Avoid some windows confusion by using 'Path', which renders correctly cross-system

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -3,6 +3,8 @@ import org.junit.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class TestDocSearch {
 	@Test 
@@ -15,7 +17,10 @@ public class TestDocSearch {
 	public void testSearch() throws URISyntaxException, IOException {
     Handler h = new Handler("./technical/");
     URI rootPath = new URI("http://localhost/search?q=taxation");
-    String expect = "Found 3 paths:\n./technical/biomed/1471-2296-3-3.txt\n./technical/government/Gen_Account_Office/d01591sp.txt\n./technical/plos/journal.pbio.0020052.txt";
+    Path path1 = Paths.get("./technical/biomed/1471-2296-3-3.txt");
+    Path path2 = Paths.get("./technical/government/Gen_Account_Office/d01591sp.txt");
+    Path path3 = Paths.get("./technical/plos/journal.pbio.0020052.txt");
+    String expect = String.format("Found 3 paths:\n%s\n%s\n%s", path1, path2, path3);
     assertEquals(expect, h.handleRequest(rootPath));
 	}
 }
